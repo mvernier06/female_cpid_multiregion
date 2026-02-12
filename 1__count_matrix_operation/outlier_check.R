a46a4c5def35baaa0df762fec8d14a32c88a690a
marine.vernier <marine.vernier31@gmail.com>
2026-02-12 09:35:58 +0100

    testing total counts vs number of reads

--- 1__count_matrix_operation/outlier_check.R
+++ 1__count_matrix_operation/outlier_check.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(readODS)
 library(ggplot2)
 library(DESeq2)
+library(ggrepel)
 
 rm(list=ls())
 
@@ -12,7 +13,7 @@ setwd("/home/marinevernier/Documents/projets/")
 #### PATHS ####
 raw_counts_path <- "female_cpid_multiregion/data/2__differential_expression_analysis/raw_counts_filtered_allreg_union.csv"
 coldata_path <- "female_cpid_multiregion/data/counts_m39_M32/coldata.ods"
-plot.path <- "female_cpid_multiregion/graphs_results/1__count_matrix_operation/"
+plot.path <- "female_cpid_multiregion/graphs_results/1__count_matrix_operation/outlier_check/"
 coverage.path <- "female_cpid_multiregion/data/counts_m39_M32/coverage_per_sample.tsv"
 
 
@@ -26,6 +27,7 @@ coverage$sample <- sub(
   coverage$sample
 )
 
+setwd(plot.path)
 
 counts_long <- counts %>%
   pivot_longer(
@@ -69,18 +71,23 @@ for (gene in genes_present_testes) {
       expression_covnorm = (expression / dedup_bam_reads) * 1e6
     )
   
-  plots[[gene]] <- ggplot(
-    count_cov,
-    aes(x = reg, y = expression_covnorm, label = sample)
-  ) +
-    geom_point(size = 3, alpha = 0.8) +
-    geom_text(vjust = -0.5, size = 3) +
-    theme_classic() +
-    labs(
-      title = paste0(gene, " – expression normalisée par la couverture"),
-      x = "Région",
-      y = "CPM (dedup reads)"
-    )
+  
+    
+    p<- ggplot(
+      count_cov,
+      aes(x = reg, y = expression_covnorm, label = sample)
+      ) +
+        geom_point(size = 3, alpha = 0.8) +
+        geom_text(vjust = -0.5, size = 3) +
+        theme_classic() +
+        labs(
+          title = paste0(gene, " – expression normalisée par la couverture"),
+          x = "Région",
+          y = "CPM (dedup reads)"
+        )
+    plots[[gene]] <- p
+    filename <- paste0(gene, "_expression.png")
+    ggsave(plot=last_plot(), filename)
 }
 plots[["Thy1"]]
 
@@ -140,6 +147,8 @@ cor_summary
 
 outliers <- c("Ins.1837", "Hb.1839", "Hb.2049")
 
+lib_size <- colSums(counts)
+
 lib_df <- tibble(
   sample = names(lib_size),
   total_counts = lib_size
@@ -167,12 +176,12 @@ ggplot(lib_df,
   ) +
   theme_minimal(base_size = 10) +
   theme(
-    axis.text.y = element_text(size = 7),
+    axis.text.y = element_text(size = 4),
     axis.text.x = element_text(size = 8),
     plot.title  = element_text(size = 11, face = "bold"),
     panel.grid.major.y = element_blank()
   )
-
+ggsave(plot=last_plot(), "library_size.png")
 
 
 
@@ -205,9 +214,42 @@ ggplot(lib_df_corrected,
   ) +
   theme_minimal(base_size = 10) +
   theme(
-    axis.text.y = element_text(size = 7),
+    axis.text.y = element_text(size = 5),
     axis.text.x = element_text(size = 8),
     plot.title  = element_text(size = 11, face = "bold"),
     panel.grid.major.y = element_blank()
   )
-  
+ggsave(plot=last_plot(), "corrected_library_size.png")  
+
+df <- tibble(
+  sample = names(lib_size),
+  total_counts = lib_size
+) %>%
+  left_join(coverage, by = "sample") 
+
+ggplot(df, 
+       aes(x = fastq_reads ,
+       y = total_counts))+
+  geom_point() +
+  geom_text_repel(
+    data = subset(df, sample %in% c("Ins.1837", "Hb.1839", "Hb.2049", "Ins.2073", "Ins.2088", "Ins.1850", "Ins2079", "Nac.1837", "Ins.2072", "Ins.2079", "Ins.1834", "Hb.2074", "Ins.1840")),
+    aes(label = sample),
+    size = 4,
+    fontface = "bold"
+  ) +
+  labs(
+       title = "Total library size and total fatsq reads",
+       x = "fastq reads",
+       y = "total counts"
+     ) 
+ggsave(plot=last_plot(), "total_counts_vs_number_reads.png")
+
+#########################################################################################################
+###################  test avec le RIN ##################
+
+
+
+
+
+
+
