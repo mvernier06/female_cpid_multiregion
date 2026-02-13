bcb3230302ce41a34fc8c2f35d6bad9775352c08
marine.vernier <marine.vernier31@gmail.com>
2026-02-13 15:26:14 +0100

    saving comparison male female

--- 0_comparison_male_female/comparison_male_female.R
+++ 0_comparison_male_female/comparison_male_female.R
@@ -11,7 +11,7 @@ rm(list=ls())
 
 counts_female <- "/home/marinevernier/Documents/projets/female_cpid_multiregion/data/2__differential_expression_analysis/raw_counts_filtered_allreg_union.csv"
 counts_male <- "/home/marinevernier/Documents/projets/cpid_multiregion/data/2__differential_expression_analysis/raw_counts_filtered_allreg_union.csv"
-coldata_female_path <- "/home/marinevernier/Documents/projets/female_cpid_multiregion/data/count_data/coldata.ods"
+coldata_female_path <- "/home/marinevernier/Documents/projets/female_cpid_multiregion/data/counts_m39_M32/coldata_without_outliers.ods"
 coldata_male_path <- "/home/marinevernier/Documents/projets/cpid_multiregion/data/2__differential_expression_analysis/coldata.csv"
 output_path <- "/home/marinevernier/Documents/projets/female_cpid_multiregion/graphs_results/0_comparison_male_female/"
 setwd(output_path)
@@ -27,27 +27,25 @@ coldata_female <- coldata_female %>%
 coldata_male <- coldata_male %>%
   mutate(sex = "male")
 
-df_female <- df_female %>%
-  column_to_rownames("MGI.symbol")  
-
-df_male <- df_male %>%
-  column_to_rownames("MGI.symbol")
+df_female <- df_female %>% column_to_rownames("MGI.symbol")
+df_male <- df_male %>% column_to_rownames("MGI.symbol")
 
 common_genes <- intersect(rownames(df_female), rownames(df_male))
+
 
 counts_all <- cbind(
   df_female[common_genes, ],
   df_male[common_genes, ]
 )
 
-coldata_all <- bind_rows(coldata_female, coldata_male) %>%
-  mutate(
-    reg = if_else(reg == "Nac", "NAc", reg)
-  ) %>%
-  column_to_rownames("sample")
-unique(coldata_all$reg)
 
-all(colnames(counts_all) == rownames(coldata_all))
+coldata_all <- bind_rows(coldata_female, coldata_male) %>%
+  mutate(reg = if_else(reg == "Nac", "NAc", reg)) %>%
+  column_to_rownames("sample")
+
+# Vérification
+stopifnot(all(colnames(counts_all) == rownames(coldata_all)))
+
 
 dds <- DESeqDataSetFromMatrix(
   countData = counts_all,
@@ -57,53 +55,69 @@ dds <- DESeqDataSetFromMatrix(
 
 vsd <- vst(dds, blind = TRUE)
 
-pca <- prcomp(t(assay(vsd)), scale. = FALSE)
 
-pca_df <- as.data.frame(pca$x) %>%
+### PCA 1 : toutes les régions ###
+
+pca_all <- prcomp(t(assay(vsd)), scale. = FALSE)
+pca_all_df <- as.data.frame(pca_all$x) %>%
   rownames_to_column("sample") %>%
-  left_join(
-    coldata_all %>% rownames_to_column("sample"),
-    by = "sample"
-  )
+  left_join(coldata_all %>% rownames_to_column("sample"), by = "sample")
 
-ggplot(pca_df, aes(PC1, PC2, color = reg, shape = sex)) +
+ggplot(pca_all_df, aes(PC1, PC2, color = reg, shape = sex)) +
   geom_point(size = 3) +
   theme_classic() +
   labs(
-    title = "PCA colored by regions, points by sex",
-    x = paste0("PC1 (", round(100*summary(pca)$importance[2,1], 1), "%)"),
-    y = paste0("PC2 (", round(100*summary(pca)$importance[2,2], 1), "%)") # ces deux lignes servent à afficher le pourcentage de variance expliquée par les PC 
-)
-ggsave(plot = last_plot(), "PCA_region_sex_all.png")
-summary(pca)
-ggplot(pca_df, aes(PC1, PC2, color = group)) +
+    title = "PCA sur toutes les régions",
+    x = paste0("PC1 (", round(100*summary(pca_all)$importance[2,1], 1), "%)"),
+    y = paste0("PC2 (", round(100*summary(pca_all)$importance[2,2], 1), "%)")
+  )
+
+ggsave(plot = last_plot(), filename = "PCA_all_reg.png", width = 6, height = 5)
+
+
+### PCA 2 : régions présentes dans les deux sexes ###
+
+
+regions_common <- coldata_all %>%
+  group_by(reg) %>%
+  summarise(sexes = n_distinct(sex)) %>%
+  filter(sexes == 2) %>%
+  pull(reg)
+
+
+coldata_common <- coldata_all %>% filter(reg %in% regions_common)
+vsd_common <- vsd[, rownames(coldata_common)]
+
+# PCA sur régions communes
+pca_common <- prcomp(t(assay(vsd_common)), scale. = FALSE)
+pca_common_df <- as.data.frame(pca_common$x) %>%
+  rownames_to_column("sample") %>%
+  left_join(coldata_common %>% rownames_to_column("sample"), by = "sample")
+
+ggplot(pca_common_df, aes(PC1, PC2, color = reg, shape = sex)) +
+  geom_point(size = 3) +
+  theme_classic() +
+  labs(
+    title = "PCA sur régions communes aux deux sexes",
+    x = paste0("PC1 (", round(100*summary(pca_common)$importance[2,1], 1), "%)"),
+    y = paste0("PC2 (", round(100*summary(pca_common)$importance[2,2], 1), "%)")
+  )
+
+ggsave(plot = last_plot(), filename = "PCA_common_reg.png", width = 6, height = 5)
+
+
+
+
+ggplot(pca_common_df, aes(PC1, PC2, color = group)) +
   geom_point(size = 3) +
   facet_wrap(~ sex) +
   theme_classic()
-ggsave(plot = last_plot(), "PCA_group_male_vs_female.png")
+ggsave(plot = last_plot(), "PCA_common_reg_group_male_vs_female.png")
 
-ggplot(pca_df, aes(PC1, PC2, color = sex)) +
+ggplot(pca_common_df, aes(PC1, PC2, color = sex)) +
   geom_point(size = 3) +
   theme_classic()
-ggsave(plot = last_plot(), "PCA_sex.png")
-
-# En affichant seulement les regions communes : 
-regions_communes <- pca_df %>%
-  distinct(reg, sex) %>%
-  dplyr::count(reg) %>%
-  filter(n == 2) %>%
-  pull(reg)
-pca_df_filt <- pca_df %>%
-  filter(reg %in% regions_communes)
-ggplot(pca_df_filt, aes(PC1, PC2, color = reg, shape = sex)) +
-  geom_point(size = 3) +
-  theme_classic() +
-  labs(
-    title = "PCA displaying only commune regions",
-    x = paste0("PC1 (", round(100*summary(pca)$importance[2,1], 1), "%)"),
-    y = paste0("PC2 (", round(100*summary(pca)$importance[2,2], 1), "%)")
-  )
-ggsave(plot = last_plot(), "PCA_commune_regions.png")
+ggsave(plot = last_plot(), "PCA_common_reg_sex.png")
 
 
 ##################################################################################################################################
@@ -208,8 +222,8 @@ ggsave(plot = last_plot(), "Uty_boxplot_male_female.png")
 
 ######### Même chose mais en prenant les counts avant filtrage #############
 
-df_unfiltered_female <- read_csv("/home/marinevernier/Documents/cpid_multiregion/female_cpid_multiregion/data/2__differential_expression_analysis/annotated_counts.csv")
-df_unfiltered_male <- read_csv("/home/marinevernier/Documents/cpid_multiregion/cpid_multiregion/data/2__differential_expression_analysis/annotated_counts.csv")
+df_unfiltered_female <- read_csv("/home/marinevernier/Documents/projets/female_cpid_multiregion/data/2__differential_expression_analysis/annotated_counts.csv")
+df_unfiltered_male <- read_csv("/home/marinevernier/Documents/projets/cpid_multiregion/data/2__differential_expression_analysis/annotated_counts.csv")
 
 coldata_female <- coldata_female %>%
   mutate(sex = "female")
