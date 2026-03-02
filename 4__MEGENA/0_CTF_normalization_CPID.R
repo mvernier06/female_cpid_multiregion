1c8a86ed0177734d08736337ac91a4caf2f2fa1d
Marine Vernier <mvernier@hpc-login1.hpc>
2026-03-02 14:04:44 +0100

    adding RIN correction with normalization

--- 4__MEGENA/0_CTF_normalization_CPID.R
+++ 4__MEGENA/0_CTF_normalization_CPID.R
@@ -1,15 +1,20 @@
+# install.packages("readODS", repos="https://cloud.r-project.org")
 library(tidyverse)
 library(edgeR)
+library(limma)
+library(readODS)
 
 setwd("/home2020/home/inci/mvernier/cpid_multireg_female/female_cpid_multiregion/")
+
 
 regionlist <- c("ACC", "Hb", "Ins", "Nac")
 
 # PATHS ####
 annotated_counts.path <- "data/2__differential_expression_analysis/annotated_counts.csv" # raw counts
+coldata.path <- "data/counts_m39_M32/coldata_without_outliers.ods" # pour récpérer les RIN et les groupes
 output.path <- "data/4__MEGENA/" # folder to save results
 dir.create(output.path)
-
+reg <- "Hb"
 for(reg in regionlist){
   # load unnormalised counts as dataframe 
   annotated_counts <- read_csv(annotated_counts.path)
@@ -18,16 +23,33 @@ for(reg in regionlist){
     column_to_rownames("MGI.symbol") %>%
     select(contains(paste0(reg, ".")))
   
+  # load coldata
+  cldata <- read_ods(coldata.path)
+  
   # keep genes with at least 1 cpm in >= 20% of samples in a single project
   cpm <- apply(counts, 2, function(x) (x/sum(x))*1000000)
   counts_filtered <- counts[rowSums(cpm < 1) < (dim(cpm)[2]/5),]
   
-  # CTF normalization
-  lib_size <- base::colSums(counts_filtered)
-  norm_factors <- calcNormFactors(object = counts_filtered, lib.size = lib_size, method = "TMM")
-  CTF_normalized <- sweep(counts_filtered, 2, norm_factors, "/")
+  # TMM normalization
+  dge <- DGEList(counts = counts_filtered)
+  dge <- calcNormFactors(dge, method = "TMM")
+  
+  # LOG2 CPM 
+  logCPM <- cpm(dge, log = TRUE)
+
+  # Correction du RIN (en protegeant effet cuff/sham)
+  samples_region <- colnames(logCPM)
+  cldata_df <- as.data.frame(cldata)
+  rownames(cldata_df) <- cldata_df$sample
+  cldata_region <- cldata_df[samples_region, ]
+  
+  design <- model.matrix(~ group, data = cldata_region)
+  
+  logCPM_corrected <- removeBatchEffect(logCPM,
+                                        covariates = cldata_region$RIN,
+                                        design = design)
   
   # save R object of normalized counts
-  filename <- paste0("CTF_normalized_counts_", reg, ".Rdata")
-  save(CTF_normalized, file=paste0(output.path, filename))
+  filename <- paste0("logCPM_RINcorrected_", reg, ".Rdata")
+  save(logCPM_corrected, file = paste0(output.path, filename))
 }
--- 4__MEGENA/1_correlationMatrix.R
+++ 4__MEGENA/1_correlationMatrix.R
@@ -6,21 +6,21 @@ library(edgeR)
 
 setwd("/home2020/home/inci/mvernier/cpid_multireg_female/female_cpid_multiregion/")
 
-# Choose a region 
+# Choose a region (ACC, Hb, Ins, Nac)
 reg <- "Hb"
 
 # PATHS ####
-CTF_normalized.path <- paste0("data/4__MEGENA/CTF_normalized_counts_", reg, ".Rdata") # CTF normalized counts for the queried region
+logCPM_RINcorrected.path <- paste0("data/4__MEGENA/logCPM_RINcorrected_", reg, ".Rdata") # CTF normalized counts for the queried region
 output.path <- paste0("data/4__MEGENA/ijw_", reg, ".Rdata") # file to save results
 
 # import CTF normalized counts
-load(CTF_normalized.path)
+load(logCPM_RINcorrected.path)
 
 # input parameters
 cor.perm = 10 # number of permutations for calculating FDRs for all correlation pairs.
 
 # generate correlation matrix
-ijw <- calculate.correlation(CTF_normalized,
+ijw <- calculate.correlation(logCPM_corrected,
                              doPerm = cor.perm,
                              output.corTable = FALSE,
                              output.permFDR = FALSE)
