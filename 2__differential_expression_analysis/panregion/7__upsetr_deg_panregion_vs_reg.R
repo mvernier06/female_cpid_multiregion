8b345b2a7622edf85216bacd09d614e425140b59
marine.vernier <marine.vernier31@gmail.com>
2026-03-19 16:05:39 +0100

    adding upset plot panregion

--- 2__differential_expression_analysis/panregion/7__upsetr_deg_panregion_vs_reg.R
+++ 2__differential_expression_analysis/panregion/7__upsetr_deg_panregion_vs_reg.R
@@ -1,68 +1,85 @@
 #### UPSETR COMPARAISON DES DEG PANREGIONS = DEG PAR REGION POUR VOIR LES INTERACTIONS ENTRE LES DEG ####
 
+rm(list=ls())
+
 #### LIBRARIES ####
 library(dplyr)
 library(UpSetR)
 
 #### PATHS #### 
-## INPUT ##
-# A modifié et mettre le ficheir avec tous les tp pour panregion 
-panregion_deg.path <- "~/CPID_multiregion/data/2__differential_expression_analysis/panregion/deseq2/design_reg_group/annotation_deg_rg/panregion_deg_rg_tp1.rds"
-deglist.path <- "~/CPID_multiregion/data/2__differential_expression_analysis/deglist.Rdata"
+project_path <- "/home/marinevernier/Documents/projets/female_cpid_multiregion/"
+setwd(project_path)
 
-## OUTPUT ## 
-upset.path <- "~/CPID_multiregion/graphs_results/panregion/deseq2_reg_group/upset_plots/upset_deg_panregion_vs_sr_tp1.png"
+deglist.path <- "data/2__differential_expression_analysis/deglist.Rdata"
 
-#### DEG DF #### 
-### PANREGION ###
-panregion_deg <- readRDS(panregion_deg.path)
 
-##LABEL UP AND DOWN ##
-panregion_up <-  panregion_deg %>% filter(diffexpressed == "UP")
-panregion_down <-  panregion_deg %>% filter(diffexpressed == "DOWN")
-
-### REGIONS (SINGLE REGIONS) ### 
 load(deglist.path)
 
-##LABEL UP AND DOWN ##
-BLA_up   <- deg_BLA_tp1 %>% filter(diffexpressed == "UP")
-BLA_down <- deg_BLA_tp1 %>% filter(diffexpressed == "DOWN")
-DRN_up   <- deg_DRN_tp1 %>% filter(diffexpressed == "UP")
-DRN_down <- deg_DRN_tp1 %>% filter(diffexpressed == "DOWN")
-Hb_up    <-  deg_Hb_tp1 %>% filter(diffexpressed == "UP")
-Hb_down  <-  deg_Hb_tp1 %>% filter(diffexpressed == "DOWN")
-Ins_up   <- deg_Ins_tp1 %>% filter(diffexpressed == "UP")
-Ins_down <- deg_Ins_tp1 %>% filter(diffexpressed == "DOWN")
-NAc_up   <- deg_NAc_tp1 %>% filter(diffexpressed == "UP")
-NAc_down <- deg_NAc_tp1 %>% filter(diffexpressed == "DOWN")
-VTA_up   <- deg_VTA_tp1 %>% filter(diffexpressed == "UP")
-VTA_down <- deg_VTA_tp1 %>% filter(diffexpressed == "DOWN")
+timepoints <- c("tp1", "tp2", "tp3")
 
-
-#### LIST UPSET ####
-listInput <- list(panregion_up   = panregion_up$label,
-                  panregion_down = panregion_down$label,
-                  BLA_up   = BLA_up$label,
-                  BLA_down = BLA_down$label,
-                  DRN_up   = DRN_up$label,
-                  DRN_down = DRN_down$label,
-                  Hb_up    = Hb_up$label,
-                  Hb_down  = Hb_down$label,
-                  Ins_up   = Ins_up$label,
-                  Ins_down = Ins_down$label,
-                  NAc_up   = NAc_up$label,
-                  NAc_down = NAc_down$label,
-                  VTA_up   = VTA_up$label,
-                  VTA_down = VTA_down$label
-)
-
-#### UPSET PLOT ####
-upset_plot <- (upset(fromList(listInput), 
-                     mainbar.y.label = "DEGs Intersections", 
-                     sets.x.label = "Number of DEGs", order.by="freq", nsets = 14))
-
-
-#### SAVE ####
-png(upset.path, width = 3000, height = 2400, res = 300)
-upset_plot
-dev.off() 
+for (tp in timepoints) {
+  
+  #### INPUT ####
+  panregion_deg.path <- paste0(
+    "data/2__differential_expression_analysis/panregion/deseq2/design_reg_group/annotation_deg/panregion_deg_", tp, ".rds"
+  )
+  
+  #### OUTPUT ####
+  upset.path <- paste0(
+    "graphs_results/panregion/deseq2_reg_group/upset_plots/upset_deg_panregion_vs_sr_", tp, ".png"
+  )
+  
+  #### LOAD PANREGION ####
+  panregion_deg <- readRDS(panregion_deg.path)
+  
+  panregion_up   <- panregion_deg %>% filter(diffexpressed == "UP")
+  panregion_down <- panregion_deg %>% filter(diffexpressed == "DOWN")
+  
+  #### LOAD DEG REGIONS ####
+  ACC <- get(paste0("deg_ACC_", tp))
+  Hb  <- get(paste0("deg_Hb_", tp))
+  Ins <- get(paste0("deg_Ins_", tp))
+  Nac <- get(paste0("deg_Nac_", tp))
+  
+  #### SPLIT UP / DOWN ####
+  ACC_up   <- ACC %>% filter(diffexpressed == "UP")
+  ACC_down <- ACC %>% filter(diffexpressed == "DOWN")
+  
+  Hb_up    <- Hb %>% filter(diffexpressed == "UP")
+  Hb_down  <- Hb %>% filter(diffexpressed == "DOWN")
+  
+  Ins_up   <- Ins %>% filter(diffexpressed == "UP")
+  Ins_down <- Ins %>% filter(diffexpressed == "DOWN")
+  
+  Nac_up   <- Nac %>% filter(diffexpressed == "UP")
+  Nac_down <- Nac %>% filter(diffexpressed == "DOWN")
+  
+  #### LIST INPUT ####
+  listInput <- list(
+    panregion_up   = panregion_up$label,
+    panregion_down = panregion_down$label,
+    ACC_up   = ACC_up$label,
+    ACC_down = ACC_down$label,
+    Hb_up    = Hb_up$label,
+    Hb_down  = Hb_down$label,
+    Ins_up   = Ins_up$label,
+    Ins_down = Ins_down$label,
+    Nac_up   = Nac_up$label,
+    Nac_down = Nac_down$label
+  )
+  
+  #### PLOT ####
+  png(upset.path, width = 3000, height = 2400, res = 300)
+  
+  print(
+    upset(
+      fromList(listInput),
+      mainbar.y.label = paste("DEGs Intersections -", tp),
+      sets.x.label = "Number of DEGs",
+      order.by = "freq",
+      nsets = length(listInput)
+    )
+  )
+  
+  dev.off()
+}
