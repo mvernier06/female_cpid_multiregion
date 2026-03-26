4f8084e341011d0b3d4a3781756a1b0cd4d40a01
Marine Vernier <mvernier@hpc-login1.hpc>
2026-03-26 09:52:51 +0100

    changing MEGENA folder organization

---
+++ 4__MEGENA/MEGENA_with_RIN_correction/module_preservation_between_regions/9_module_preservation_between_regions.R
@@ -0,0 +1,151 @@
+rm(list = ls()) # rm R working space
+
+library(WGCNA)
+library(tidyverse)
+
+project.path <- "/home2020/home/inci/mvernier/cpid_multireg_female/"
+setwd(project.path)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+reg1 <- args[1]
+reg2 <- args[2]
+
+cat("Running:", reg1, "vs", reg2, "\n")
+
+
+enableWGCNAThreads(nThreads = 8)
+
+# -------- PATHS --------
+base_modules_path <- "female_cpid_multiregion/data/4__MEGENA/MEGENA_with_RIN_correction/MEGENA.Results_"
+base_counts_path  <- "female_cpid_multiregion/data/4__MEGENA/MEGENA_with_RIN_correction/logCPM_RINcorrected_"
+
+output_data_base  <- "female_cpid_multiregion/data/4__MEGENA/MEGENA_with_RIN_correction/preservation_between_regions/"
+output_graph_base <- "female_cpid_multiregion/graphs_results/4__MEGENA/MEGENA_with_RIN_correction/preservation_between_regions/"
+
+dir.create(output_data_base, recursive = TRUE, showWarnings = FALSE)
+dir.create(output_graph_base, recursive = TRUE, showWarnings = FALSE)
+
+# -------- FUNCTION --------
+MEGENA_to_moduleColors <- function(module_list) {
+  module_list <- module_list[order(sapply(module_list, length))]
+  moduleColors <- c()
+  
+  for (mod in names(module_list)) {
+    genes <- module_list[[mod]]
+    genes <- setdiff(genes, names(moduleColors))
+    moduleColors[genes] <- mod
+  }
+  
+  return(moduleColors)
+}
+
+# -------- LOAD REGIONS --------
+load_region <- function(reg) {
+  env <- new.env()
+  
+  load(paste0(base_modules_path, reg, ".Rdata"), envir = env)
+  load(paste0(base_counts_path, reg, ".Rdata"))
+  
+  moduleColors <- MEGENA_to_moduleColors(env$MEGENA.output$module.output$modules)
+  
+  return(list(
+    moduleColors = moduleColors,
+    counts = logCPM_corrected
+  ))
+}
+
+data1 <- load_region(reg1)
+data2 <- load_region(reg2)
+
+# -------- COMMON GENES --------
+common_genes <- Reduce(intersect, list(
+  names(data1$moduleColors),
+  names(data2$moduleColors),
+  rownames(data1$counts),
+  rownames(data2$counts)
+))
+cat("Number of common genes:", length(common_genes), "\n")
+
+counts1 <- data1$counts[common_genes, ]
+counts2 <- data2$counts[common_genes, ]
+
+moduleColors1 <- data1$moduleColors[common_genes]
+moduleColors2 <- data2$moduleColors[common_genes]
+
+datExpr1 <- t(counts1)
+datExpr2 <- t(counts2)
+
+multiExpr <- list(
+  reg1 = list(data = datExpr1),
+  reg2 = list(data = datExpr2)
+)
+
+# ==============================
+#  REG1 → REG2
+# ==============================
+mp1 <- modulePreservation(
+  multiExpr,
+  colorList = list(reg1 = moduleColors1),
+  referenceNetworks = 1,
+  nPermutations = 100,
+  randomSeed = 123
+)
+
+pres1 <- mp1$preservation$Z$ref.reg1$inColumnsAlsoPresentIn.reg2
+obs1  <- mp1$preservation$observed$ref.reg1$inColumnsAlsoPresentIn.reg2
+
+res1 <- data.frame(
+  module = rownames(pres1),
+  Zsummary = pres1[, "Zsummary.pres"],
+  size = obs1[, "moduleSize"],
+  medianRank = obs1[, "medianRank.pres"],
+  ref = reg1,
+  test = reg2
+)
+
+# ==============================
+# REG2 → REG1
+# ==============================
+mp2 <- modulePreservation(
+  multiExpr,
+  colorList = list(reg2 = moduleColors2),
+  referenceNetworks = 2,
+  nPermutations = 100,
+  randomSeed = 123
+)
+
+pres2 <- mp2$preservation$Z$ref.reg2$inColumnsAlsoPresentIn.reg1
+obs2  <- mp2$preservation$observed$ref.reg2$inColumnsAlsoPresentIn.reg1
+
+res2 <- data.frame(
+  module = rownames(pres2),
+  Zsummary = pres2[, "Zsummary.pres"],
+  size = obs2[, "moduleSize"],
+  medianRank = obs2[, "medianRank.pres"],
+  ref = reg2,
+  test = reg1
+)
+
+# -------- MERGE --------
+df <- rbind(res1, res2)
+
+# -------- SAVE --------
+save(df, file = paste0(output_data_base, reg1, "_vs_", reg2, ".Rdata"))
+
+
+  
+# ---------- PLOTS ----------
+p <- ggplot(df, aes(x = Zsummary, fill = direction)) +
+    geom_density(alpha = 0.4) +
+    geom_vline(xintercept = 2, linetype = "dashed") +
+    geom_vline(xintercept = 10, linetype = "dashed") +
+    theme_minimal() +
+    ggtitle(paste("Zsummary:", reg1, "vs", reg2))
+
+ggsave(
+filename = paste0(output_graph_base, reg1, "_vs_", reg2, "_density.png"),
+plot = p
+)
+
+cat("Finished:", reg1, "vs", reg2, "\n")
---
+++ 4__MEGENA/scripts_sbatch/job_R_parall_preservation_regions.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+#SBATCH --time=0-24:00:00
+# SBATCH -p public
+#SBATCH -p grant -A g2025a419c
+#SBATCH --output=run_R_job%J.out
+#SBATCH --error=run_R_job%J.err
+#SBATCH --mail-type=END  
+#SBATCH --mail-user=marinevernier@unistra.fr
+#SBATCH -N 1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=50G
+#SBATCH --array=1-6
+
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate r_megena
+
+checktime=$(date);
+echo "start time $checktime";
+
+regions=("ACC" "Ins" "Hb" "Nac")
+
+pairs=(
+"ACC Ins"
+"ACC Hb"
+"ACC Nac"
+"Ins Hb"
+"Ins Nac"
+"Hb Nac"
+)
+
+pair=${pairs[$SLURM_ARRAY_TASK_ID-1]}
+
+reg1=$(echo $pair | cut -d' ' -f1)
+reg2=$(echo $pair | cut -d' ' -f2)
+
+echo "Running $reg1 vs $reg2"
+
+Rscript /home2020/home/inci/mvernier/cpid_multireg_female/female_cpid_multiregion/4__MEGENA/MEGENA_without_RIN_correction/9_module_preservation_between_regions.R $reg1 $reg2
+checktime=$(date);
+echo "end time $checktime"; 
+
+exit 0
