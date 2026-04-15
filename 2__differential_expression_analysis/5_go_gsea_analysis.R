ee91b47589804646afb960b38f4dd0f60d843ec8
marine.vernier <marine.vernier31@gmail.com>
2026-04-15 17:18:43 +0200

    Adding fgsea with pvalue cuttof 0.1

--- 2__differential_expression_analysis/5_go_gsea_analysis.R
+++ 2__differential_expression_analysis/5_go_gsea_analysis.R
@@ -135,3 +135,69 @@ for(reg in regionList){
   }
 }
 
+## pareil en mettant pvalueCutoff 0.1 (pour tester les overlap de terms : script intersection_go_terms_male_female.R )
+set.seed(123)
+for(reg in regionList){
+  for(tp in tpList){
+    message("Doing GSEA analysis of ", reg, " for TP", tp)
+    
+    counts_temp <- counts %>%
+      dplyr::select(MGI.symbol, 
+                    matches(paste0("^", reg, "_log2fc_tp", tp, "$")), 
+                    matches(paste0("^", reg, "_pval_tp", tp, "$"))) %>%
+      na.omit()
+    
+    #print(colnames(counts_temp))  # Vérifier les colonnes sélectionnées
+    #print(dim(counts_temp))  # Vérifier le nombre de lignes et colonnes
+    
+    if (ncol(counts_temp) == 3) {
+      colnames(counts_temp) <- c("MGI.symbol", "log2fc", "pvalue")
+    } else {
+      print(" Problème de sélection des colonnes")
+      print(colnames(counts_temp))
+      next
+    }
+    
+    # Ranking
+    counts_temp <- counts_temp %>%
+      mutate(rank_score = -log10(pvalue) * sign(log2fc))  # score for ranking
+    
+    # Sort according to ranking score 
+    counts_temp_sorted <- counts_temp %>%
+      arrange(desc(rank_score))  # sorting 
+    
+    # gene list of the sorted gene set 
+    genelist <- setNames(counts_temp_sorted$rank_score, counts_temp_sorted$MGI.symbol)
+    
+    
+    # GSEA
+    gse <- gseGO(geneList = genelist, 
+                 ont = "ALL", 
+                 keyType = "SYMBOL", 
+                 minGSSize = 3, 
+                 maxGSSize = 800, 
+                 eps = 1e-300,
+                 pvalueCutoff = 0.1, 
+                 verbose = TRUE, 
+                 OrgDb = organism, 
+                 pAdjustMethod = "BH", 
+                 by = "fgsea",
+                 seed = 123)
+    
+    assign(paste("fgsea_01", reg, tp, sep = "_"), gse)
+  }
+}
+
+## SAVE ##
+rm(gse)
+gse_obj <- ls()[grepl("fgsea_01_",ls())]
+save(list=gse_obj, file= paste0(output.path, "fgsea_01_obj.Rdata"))
+
+
+## COUNT RESULT ##
+for(reg in regionList){
+  for(tp in tpList){
+    gse_obj <- paste("fgsea_01", reg, tp, sep="_")
+    print(paste0(reg, " tp", tp, ": ",nrow(get(gse_obj))))
+  }
+}
